d985e8a717c7fb1efb43360fe5d7ccacb0e57f0a
fatah <fkacm2004@gmail.com>
2024-03-03 03:11:21 +0100

    first ct

--- srcVercelniceWebApp/redux/store.js
+++
@@ -1,7 +0,0 @@
-// redux/store.js
-import { createStore } from 'redux';
-import counterReducer from './reducer';
-const store = createStore(counterReducer);
-//,window.__REDUX_DEVTOOLS_EXTENSION__()
- //window.__REDUX_DEVTOOLS_EXTENSION__.open();
-export default store;
